db2487bea8fe978bf768bde3fc9a1a560c0b4fbc
Courey Elliott <coureyelliott@gmail.com>
2014-09-10 10:53:45 -0500

    added test for uniqueness of name in category spec

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
   validates_presence_of :name
-  validates_uniqueness_of :name, message: "This category already exists."
+  validates_uniqueness_of :name, message: "has already been taken"
   has_many :services
 end
