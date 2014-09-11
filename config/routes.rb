5910390227fe3ff45992778ef04c6e68f935459a
Courey Elliott <coureyelliott@gmail.com>
2014-09-11 10:15:20 -0500

    added show route, view, and controller to service model

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :categories, only: [:index, :show]
+  resources :services, only: [:show]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
