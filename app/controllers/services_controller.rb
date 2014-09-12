219b15229d6a3c7304a97932f059d9d16d983e6e
Courey Elliott <coureyelliott@gmail.com>
2014-09-12 12:42:58 -0500

    added map to individual service show page

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,10 @@
 class ServicesController < ApplicationController
   def show
     @service = Service.find(params[:id])
+    @hash = Gmaps4rails.build_markers(@service) do |service, marker|
+      marker.lat service.latitude
+      marker.lng service.longitude
+      marker.infowindow service.name
+    end
   end
 end
